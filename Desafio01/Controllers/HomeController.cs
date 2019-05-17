804b23bea320d795c9aef50faf6bcf4df0a7a1d5
mfilardi3 <mfilardi3@gmail.com>
2019-05-17 18:06:53 -0300

    correção erro

---
+++ Desafio01/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Desafio_Attributor.Models;
+
+namespace Desafio_Attributor.Controllers
+{
+    //controlador da Home
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(); //retorna exibição da view Index
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Startup de tecnologia especializada em recuperação de impostos para empresas do Simples Nacional.";
+
+            return View(); //retorna exibição da view About
+        }
+
+        public ActionResult Contact()
+        {
+            Infos obj = new Infos();
+            obj.Faculdade = "IME";
+            obj.Empresa = "Attributor";
+
+            return View(obj); //retorna exibição da view Contact
+        }
+    }
+}
---
+++ Desafio01/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas através do seguinte
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Desafio01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Desafio01")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como falso torna não visíveis os tipos neste assembly
+// para componentes COM. Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("dc21b1fc-ecb6-46ae-972d-dff9a2f95639")]
+
+// As informações de versão de um assembly consistem nos seguintes quatro valores:
+//
+//      Versão Principal
+//      Versão Secundária
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
